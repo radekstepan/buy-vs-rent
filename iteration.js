ed8601801550ddf24de03c1fa865f2e0759e9dd7
Radek Stepan <dev@radekstepan.com>
2017-11-17 23:25:12 -0500

    split iteration off

---
+++ iteration.js
@@ -0,0 +1,118 @@
+module.exports = (opts, emit) => {
+  let property_value = opts.property_value; // house value right now
+  let property_value_yearly = opts.property_value; // yearly house price
+  let property_tax = null;
+  let property_maintenance = opts.property_maintenance;
+
+  let income = opts.income / 12; // monthly income for this year
+  let income_tax = opts.income_tax; // income tax for this year
+  let expenses = opts.expenses; // monthly expenses for this year
+
+  let rent = opts.rent; // monthly rent for this year
+  let mortgage = null;
+  let mortgage_rate = opts.mortgage_rate();
+  let mortgage_term = opts.mortgage_term;
+
+  let deposit = opts.savings; // $ deposit saved so far
+  let stock = { // stock market balance
+    rent: 0,
+    buy: 0
+  };
+  let paid_off = false; // when is this house paid off?
+  let defaulted = false;
+  let equity = 0; // amount of monies paid off on mortgage already
+
+  let stop = false;
+  let year = 1;
+  let month = 0;
+  let now = null;
+  while (!stop) {
+    month += 1; // new month
+    now = ((year - 1) * 12) + month;
+
+    let stock_return = opts.stock_return(); // stock market return for this month
+    if (stock_return > 0) { // we'll need to pay tax on stock market profit; TODO RRSP
+      stock_return *= 1 - income_tax;
+    }
+
+    let available = (income * (1 - income_tax)) - expenses;
+    stock.rent = (stock.rent + available - rent) * (1 + stock_return); // stock in rent condition, simples...
+
+    property_value *= 1 + opts.property_appreciation(); // new property value
+
+    if (!defaulted) {
+      // Saving for house deposit.
+      if (!paid_off) {
+        const deposit_amount = opts.mortgage_deposit(property_value);
+        const cmhc_insurance = opts.mortgage_insurance(property_value, deposit_amount);
+        const deposit_needed = property_value * (deposit_amount + opts.property_transaction_fees); // this is how much I need to save
+        deposit += available - rent; // saved up more
+
+        // Saved up enough? Buy!
+        if (deposit >= deposit_needed) {
+          mortgage = cmhc_insurance - (deposit - deposit_needed) + (property_value * (1 - deposit_amount));
+          paid_off = now + (mortgage_term * 12);
+          property_tax = property_value * opts.property_tax;
+          equity -= cmhc_insurance;
+        }
+      } else {
+        let mortgage_payment = 0;
+
+        // Paying off mortgage?
+        if (paid_off > now) {
+          if ((now % (5 * 12)) === 0) mortgage_rate = opts.mortgage_rate(); // adjust mortgage rate after 5 years?
+          mortgage_payment = opts.mortgage_payment(mortgage, mortgage_rate, mortgage_term);
+        }
+        const maint = (property_value_yearly * property_maintenance) / 12; // property maintenance for this month
+
+        available -= mortgage_payment + (property_tax / 12) + maint; // available to invest
+        equity += mortgage_payment;
+
+        // Can't pay up?
+        if (available < 0) {
+          // Sell the house, move the monies to stock and rent again.
+          if (paid_off > now) {
+            stock.buy -= mortgage - equity; // haven't paid off mortgage yet
+          }
+          stock.buy += property_value * (1 - opts.property_transaction_fees);
+          defaulted = true;
+        } else {
+          stock.buy = (stock.buy + available) * (1 + stock_return); // adjust stock market portfolio
+        }
+      }
+    } else {
+      stock.buy = (stock.buy + available - rent) * (1 + stock_return); // just the stock market now
+    }
+
+    // A new year.
+    if (!(month % 12)) {
+      property_value_yearly = property_value; // update yearly property value
+      property_tax *= 1 + opts.property_tax_increase; // tax is higher
+      rent *= (1 + opts.rent_increase()); // rent is more expensive
+
+      income *= (1 + opts.income_increase); // I make more
+      income_tax += opts.income_tax_increase; // income tax bump
+      expenses *= (1 + opts.expenses_increase); // more expenses
+
+      year += 1; month = 0;
+
+      // Log it.
+      let v = stock.rent;
+      emit('rent', v);
+
+      if (paid_off) {
+        if (defaulted) {
+          v = stock.buy;
+        } else {
+          // Property value (less mortgage with equity paid off) and stock.
+          v = (property_value * (1 - opts.property_transaction_fees)) - mortgage + equity + stock.buy;
+        }
+      } else {
+        v = deposit;
+      }
+      emit('buy', v);
+    }
+    // Stop after x years automatically.
+    if (year > opts.years) stop = true;
+  }
+};
--- run.js
+++ run.js
@@ -5,63 +5,67 @@ const d3 = require('d3');
 const Finance = require('financejs');
 const finance = new Finance();
 
+const iteration = require('./iteration');
+
 const r = (name, map) => fs.readFileSync(`./data/${name}`, 'utf-8').split('\n').map(map);
 const n = val => numeral(val).value();
 
 // ------------------------
 
-const ITERATIONS = 1e3;
-const YEARS = 30;
+const opts = {};
 
-const INFLATION = 0.02; // desired inflation rate set by Bank of Canada
+opts.iterations = 1e3;
+opts.years = 30;
 
-const INCOME = n('100k'); // $ net yearly income
-const INCOME_INCREASE = INFLATION + 0.02; // % yearly
-const INCOME_TAX = 0.3; // %
-const INCOME_TAX_INCREASE = 0.0025; // % yearly increase to income tax (higher band etc.)
-const EXPENSES = n('2k'); // monthly expenses
-const EXPENSES_INCREASE = INFLATION; // % yearly
+opts.inflation = 0.02; // desired inflation rate set by Bank of Canada
 
-const SAVINGS = n('10k'); // monies already saved up
+opts.income = n('100k'); // $ net yearly income
+opts.income_increase = opts.inflation + 0.02; // % yearly
+opts.income_tax = 0.3; // %
+opts.income_tax_increase = 0.0025; // % yearly increase to income tax (higher band etc.)
+opts.expenses = n('2k'); // monthly expenses
+opts.expenses_increase = opts.inflation; // % yearly
 
-const RENT = n('2k'); // $ monthly
+opts.savings = n('10k'); // monies already saved up
+
+opts.rent = n('2k'); // $ monthly
 // https://www.ontario.ca/page/rent-increase-guideline
-const RENT_INCREASE = (function() { // yearly rate
+opts.rent_increase = (function() { // yearly rate
   const d = r('rent_increase.csv', parseFloat);
   return () => d[PD.rint(1, 0, d.length - 1).pop()];
 })();
 
-// const STOCK_RETURN = () => PD.rnorm(1, 0.65, 2.78).pop() / 100;
+// opts.stock_return = () => PD.rnorm(1, 0.65, 2.78).pop() / 100;
 // https://www.portfoliovisualizer.com/backtest-portfolio
-const STOCK_RETURN = (function() { // yearly rate
+opts.stock_return = (function() { // yearly rate
   const d = r('60-40_portfolio_returns.csv', parseFloat);
   return () => d[PD.rint(1, 0, d.length - 1).pop()];
 })();
 
-const PROPERTY_VALUE = n('400k'); // $
-const PROPERTY_TYPE = 'apartment'; // [ 'single_family', 'apartment' ]
-const PROPERTY_APPRECIATION = (function() { // monthly rate
-  const r = Math.pow(1 + INFLATION, 1 / 12) - 1;
+opts.property_value = n('400k'); // $
+// opts.property_type = 'apartment'; // [ 'single_family', 'apartment' ]
+opts.property_appreciation = (function() { // monthly rate
+  const r = Math.pow(1 + opts.inflation, 1 / 12) - 1;
   return () => r;
 })();
-// const PROPERTY_APPRECIATION = (function() { // monthly rate
-//   const d = r(`${PROPERTY_TYPE}_appreciation_toronto.csv`, parseFloat);
+// opts.property_appreciation = (function() { // monthly rate
+//   const d = r(`${opts.property_type}_appreciation_toronto.csv`, parseFloat);
 //   return () => d[PD.rint(1, 0, d.length - 1).pop()];
 // })();
-const PROPERTY_TAX = 0.007; // % of property value yearly
-const PROPERTY_TAX_INCREASE = INFLATION; // % yearly
-const PROPERTY_MAINTENANCE = 0.015; // % of property value earmarked yearly
-const PROPERTY_TRANSACTION_FEES = 0.06; // % transaction fees to buy/sell
+opts.property_tax = 0.007; // % of property value yearly
+opts.property_tax_increase = opts.inflation; // % yearly
+opts.property_maintenance = 0.015; // % of property value earmarked yearly
+opts.property_transaction_fees = 0.06; // % transaction fees to buy/sell
 
 // https://www.ratehub.ca/mortgage-down-payment
-const MORTGAGE_DEPOSIT = function(property_value) { // % of buy price
+opts.mortgage_deposit = function(property_value) { // % of buy price
   if (property_value <= n('1m')) {
     return (n('25k') + ((property_value - n('500k')) * 0.1)) / property_value;
   } else {
     return 0.2;
   }
 };
-const MORTGAGE_INSURANCE = function(property_value, mortgage_deposit) {
+opts.mortgage_insurance = function(property_value, mortgage_deposit) {
   const mortgage = property_value * (1 - mortgage_deposit);
   switch (true) {
     case mortgage_deposit < 0.1:
@@ -76,160 +80,45 @@ const MORTGAGE_INSURANCE = function(property_value, mortgage_deposit) {
 };
 
 // https://www.ratehub.ca/5-year-fixed-mortgage-rate-history
-const MORTGAGE_RATE = (function() { // yearly rate
+opts.mortgage_rate = (function() { // yearly rate
   const d = r('5yr_fixed_mortgage.csv', parseFloat);
   return () => d[PD.rint(1, 0, d.length - 1).pop()];
 })();
-const MORTGAGE_TERM = 25; // years
+opts.mortgage_term = 25; // years
+opts.mortgage_payment = function(mortgage, rate, term) { // monthly repayment
+  return finance.AM(mortgage, rate * 100, term * 12, 1);
+};
 
 // ------------------------
 
-const res = {
-  props: {
-    income: INCOME,
-    rent: RENT,
-    house: PROPERTY_VALUE,
-    iterations: ITERATIONS,
-    years: YEARS
-  },
-  data: {
-    buy: [],
-    rent: []
-  }
+const data = {
+  buy: [],
+  rent: []
 };
-res.data.buy.iterations = [];
-res.data.rent.iterations = [];
-for (let i = 0; i < ITERATIONS; i++) {
+data.buy.iterations = [];
+data.rent.iterations = [];
+for (let i = 0; i < opts.iterations; i++) {
   (function() {
-    res.data.buy.iterations.push([]);
-    res.data.rent.iterations.push([]);
+    data.buy.iterations.push([]);
+    data.rent.iterations.push([]);
 
-    let property_value = PROPERTY_VALUE; // house value right now
-    let property_value_yearly = PROPERTY_VALUE; // yearly house price
-    let property_tax = null;
-    let income = INCOME / 12; // monthly income for this year
-    let income_tax = INCOME_TAX; // income tax for this year
-    let expenses = EXPENSES; // monthly expenses for this year
-    let rent = RENT; // monthly rent for this year
-    let mortgage = null;
-    let mortgage_rate = MORTGAGE_RATE();
-
-    let deposit = SAVINGS; // $ deposit saved so far
-    let stock = { // stock market balance
-      rent: 0,
-      buy: 0
-    };
-    let paid_off = false; // when is this house paid off?
-    let defaulted = false;
-    let equity = 0; // amount of monies paid off on mortgage already
-
-    let stop = false;
-    let year = 1;
-    let month = 0;
-    let now = null;
-    while (!stop) {
-      month += 1; // new month
-      now = ((year - 1) * 12) + month;
-
-      let stock_return = STOCK_RETURN(); // stock market return for this month
-      if (stock_return > 0) { // we'll need to pay tax on stock market profit; TODO RRSP
-        stock_return *= 1 - income_tax;
-      }
-
-      let available = (income * (1 - income_tax)) - expenses;
-      stock.rent = (stock.rent + available - rent) * (1 + stock_return); // stock in rent condition, simples...
-
-      property_value *= 1 + PROPERTY_APPRECIATION(); // new property value
-
-      if (!defaulted) {
-        // Saving for house deposit.
-        if (!paid_off) {
-          const deposit_amount = MORTGAGE_DEPOSIT(property_value);
-          const cmhc_insurance = MORTGAGE_INSURANCE(property_value, deposit_amount);
-          const deposit_needed = property_value * (deposit_amount + PROPERTY_TRANSACTION_FEES); // this is how much I need to save
-          deposit += available - rent; // saved up more
-
-          // Saved up enough? Buy!
-          if (deposit >= deposit_needed) {
-            mortgage = cmhc_insurance - (deposit - deposit_needed) + (property_value * (1 - deposit_amount));
-            paid_off = now + (MORTGAGE_TERM * 12);
-            property_tax = property_value * PROPERTY_TAX;
-            equity -= cmhc_insurance;
-          }
-        } else {
-          let mortgage_payment = 0;
-
-          // Paying off mortgage?
-          if (paid_off > now) {
-            if ((now % (5 * 12)) === 0) mortgage_rate = MORTGAGE_RATE(); // adjust mortgage rate after 5 years?
-            mortgage_payment = finance.AM(mortgage, mortgage_rate * 100, MORTGAGE_TERM * 12, 1); // monthly repayment
-          }
-          let property_maintenance = (property_value_yearly * PROPERTY_MAINTENANCE) / 12; // property maintenance for this month
-
-          available -= mortgage_payment + (property_tax / 12) + property_maintenance; // available to invest
-          equity += mortgage_payment;
-
-          // Can't pay up?
-          if (available < 0) {
-            // Sell the house, move the monies to stock and rent again.
-            if (paid_off > now) {
-              stock.buy -= mortgage - equity; // haven't paid off mortgage yet
-            }
-            stock.buy += property_value * (1 - PROPERTY_TRANSACTION_FEES);
-            defaulted = true;
-          } else {
-            stock.buy = (stock.buy + available) * (1 + stock_return); // adjust stock market portfolio
-          }
-        }
-      } else {
-        stock.buy = (stock.buy + available - rent) * (1 + stock_return); // just the stock market now
-      }
-
-      // A new year.
-      if (!(month % 12)) {
-        property_value_yearly = property_value; // update yearly property value
-        property_tax *= 1 + PROPERTY_TAX_INCREASE; // tax is higher
-        rent *= (1 + RENT_INCREASE()); // rent is more expensive
-
-        income *= (1 + INCOME_INCREASE); // I make more
-        income_tax += INCOME_TAX_INCREASE; // income tax bump
-        expenses *= (1 + EXPENSES_INCREASE); // more expenses
-
-        year += 1; month = 0;
-
-        // Log it.
-        let v = stock.rent;
-        res.data.rent.iterations[i].push(Math.round(v));
-
-        if (paid_off) {
-          if (defaulted) {
-            v = stock.buy;
-          } else {
-            // Property value (less mortgage with equity paid off) and stock.
-            v = (property_value * (1 - PROPERTY_TRANSACTION_FEES)) - mortgage + equity + stock.buy;
-          }
-        } else {
-          v = deposit;
-        }
-        res.data.buy.iterations[i].push(Math.round(v));
-      }
-      // Stop after x years automatically.
-      if (year > YEARS) stop = true;
-    }
+    iteration(opts, (key, val) => {
+      data[key].iterations[i].push(val);
+    });
   })();
 }
 
 // Calculate quantiles.
 ['buy', 'rent'].map(key => {
-  for (let y = 0; y < YEARS; y++) {
+  for (let y = 0; y < opts.years; y++) {
     const year = [];
-    for (let i = 0; i < ITERATIONS; i++) {
-      year[i] = res.data[key].iterations[i][y];
+    for (let i = 0; i < opts.iterations; i++) {
+      year[i] = data[key].iterations[i][y];
     }
     year.sort(d3.ascending);
-    res.data[key][y] = [0.05, 0.5, 0.95].map(q => d3.quantile(year, q));
+    data[key][y] = [0.05, 0.5, 0.95].map(q => Math.round(d3.quantile(year, q)));
   }
-  delete res.data[key].iterations;
+  delete data[key].iterations;
 });
 
-fs.writeFileSync('./data.js', `const d = ${JSON.stringify(res, null, 2)};`);
+fs.writeFileSync('./data.json', JSON.stringify(data));
